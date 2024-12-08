be69a2d783a2c828b525fd8c492e737c60ac5dcc
hain1111 <136292066+hain1111@users.noreply.github.com>
2024-12-08 13:11:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const rows = 10;
-const cols = 10;
+let isPlay = false;
+let rows = 10; // 행
+let cols = 10; // 열
+let checked_nums = 0; // 확인한 셀 수
+let totalBombs = 0; // 폭탄 수
+let pro = 0.1; // 폭탄 확률
 
 // 보드 초기화
-function InitBoard() {
+function InitBoard(level) {
+  ClearBoard();
+  checked_nums = 0;
+  totalBombs = 0;
+  pro = 0.1;
+  
+  if (level === "easy") {
+    rows = 10;
+    cols = 10;
+    pro = 0.1;
+  }
+  else if (level === "medium") {
+    rows = 20;
+    cols = 20;
+    pro = 0.1;
+  }
+  else if (level === "hard") {
+    rows = 25;
+    cols = 25;
+    pro = 0.2;
+  }
+  isPlay = true;
+  
   const board = document.getElementById("board");
-  let totalBombs = 0;
+  board.style.gridTemplateColumns = `repeat(${cols}, 30px)`;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j <  cols; j++) {
       const cell = document.createElement("div");
@@ -27,12 +53,23 @@ function InitBoard() {
 
 // 셀 클릭
 function handle(row, col) {
+  if (!isPlay) return;
+  
   const cell = document.getElementById(`cell-${row}${col}`);
   const isbomb = cell.dataset.isbomb === "true";
   Neighbor(row, col);
   
+  //폭탄 클릭할 경우
   if (isbomb) {
     cell.style.color = "blue";
+    isPlay = false;
+    GameOver();
+    return;
+  }
+  
+  if (rows * cols - checked_nums == totalBombs) {
+    isPlay = false;
+    GameClear();
   }
 }
 
@@ -76,9 +113,11 @@ function Neighbor(row, col) {
   }
   
   cell.dataset.checked = "true";
+  checked_nums++;
   
   cell.textContent = `${bomb_nums}`;
-  cell.style.color = "white";
+  cell.style.color = "black";
+  cell.style.backgroundColor = "#fff";
   
   if (bomb_nums == 0) {
     for (let i = 0; i < neighbor.length; i++) {
@@ -93,5 +132,18 @@ function Neighbor(row, col) {
   return bomb_nums;
 }
 
-InitBoard();
+function ClearBoard() {
+  const board = document.getElementById("board");
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
+}
 
+function GameOver() {
+  alert("Game Over");
+}
+function GameClear() {
+  alert("Game Clear");
+}
+
+InitBoard();
